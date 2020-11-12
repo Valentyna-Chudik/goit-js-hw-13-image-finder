6908a69bb6a8dc8fed3fbf45787005d1c85a42b4
Valentyna-Chudik <valentyna_chudik@ukr.net>
2020-11-12 12:56:45 +0200

    Mentor remarks fixed

--- src/index.js
+++ src/index.js
@@ -5,20 +5,19 @@ import animateScrollTo from 'animated-scroll-to';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
-// import * as basicLightbox from 'basiclightbox';
+import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
 import imgsTpl from './templates/image-card.hbs';
 
 import NewsApiService from './js/apiService.js';
 import LoadMoreBtn from './js/load-more-btn.js';
-// import errorNotification from './js/notice-error.js';
+import errorNotification from './js/notice-error.js';
 
 
 const refs = {
     searchForm: document.querySelector('.js-search-form'),
     imgsContainer: document.querySelector('.js-gallery'),
-    
     // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 
@@ -31,19 +30,27 @@ const newsApiService = new NewsApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.refs.button.addEventListener('click', fetchImgs);
+refs.imgsContainer.addEventListener('click', onOpenModal);
 
 function onSearch(evt) {
     evt.preventDefault();
    
     newsApiService.query = evt.currentTarget.elements.query.value;
 
-    // if (newsApiService.query === '') {
-    //     return alert("Enter your query");
-    // }
-    loadMoreBtn.show();
-    newsApiService.resetPage();
-    clearImgsContainer(); 
-    fetchImgs();
+    if (newsApiService.query === '') {
+        return alert("Enter your query");
+    };
+    
+    try {
+       loadMoreBtn.show();
+        newsApiService.resetPage();
+        clearImgsContainer();
+        fetchImgs();
+       
+   } catch (error) {
+       errorNotification.noticeNoResults
+   }
+  
 };
 
 function fetchImgs() {
@@ -51,7 +58,7 @@ function fetchImgs() {
     newsApiService.fetchImgs().then(hits => {
         renderImgsMarkup(hits);
         loadMoreBtn.enable();
-        scrollTo();
+        onScrollTo();
     });
 };
 
@@ -63,7 +70,7 @@ function clearImgsContainer() {
     refs.imgsContainer.innerHTML = '';
 };
 
-function scrollTo() {
+function onScrollTo() {
    const scrollToIndex = 12 * (newsApiService.page - 1) - 11;
   const scrollToElement = refs.imgsContainer.children[scrollToIndex];
   const options = {
@@ -72,4 +79,26 @@ function scrollTo() {
   };
 
   animateScrollTo(scrollToElement, options);
+}
+
+
+// // ScrollToBottom
+// function scrollTo() {
+//    window.scrollTo({
+//   top: document.body.scrollHeight,
+//   left: 0,
+//   behavior: 'smooth'
+// });
+// }
+
+// Modal Window Opening
+function onOpenModal(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImage = `<img src= ${evt.target.dataset.source}>`;
+  basicLightbox.create(largeImage).show();
 }
